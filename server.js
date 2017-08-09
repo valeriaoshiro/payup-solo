90ae16249b79822d3cc20f8d68045ea875306d8f
Valeria Oshiro <valeriaoshiro19@gmail.com>
2017-08-09 10:59:33 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+require('config/database');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('method-override')('_method'));
 
 app.use('/', index);
 app.use('/users', users);
