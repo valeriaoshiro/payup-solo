1dee7ded8dd755e737e1c1cf58dcacb065607f8c
Rosibel Cisneros <kelsocisneros@gmail.com>
2017-08-13 00:52:15 -0700

    styled homepage

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ require('./config/database');
 require('./config/passport');
 
 var index = require('./routes/index');
+// var about = require('./routes/about');
+// var contact = require('./routes/contact');
 var users = require('./routes/users');
 var transactions = require('./routes/transactions');
 
@@ -40,8 +42,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', index);
+// app.use('/about', about);
+// app.use('/contact', contact);
 app.use('/users', users);
 app.use('/transactions', transactions);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
