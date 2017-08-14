02b6cb9d6e5ef15062cb3b71c14f82a813e9eb07
Valeria Oshiro <valeriaoshiro19@gmail.com>
2017-08-14 16:47:12 -0700

    Avatar showing

--- controllers/users.js
+++ controllers/users.js
@@ -10,26 +10,17 @@ module.exports = {
 
 function index (req, res) {
   User.find({}, function(err, users){
-    console.log(req.user.id);
     res.render('./users/index', {users: users, user: req.user.id})
   });  
 }
 
 function show (req, res) {
-  // User.findById(req.params.id).populate('transactions').exec(function(err, user){
-  //   res.render('./users/show', {user: req.user.id, userDetail: user});
-
-  // })
-
   User.findById(req.params.id, function (err, user) {
-    // res.send("Test");
     var arr = [];
     Transaction.find({}, function(err, trans){
       trans.forEach(function(tr){
-        console.log(tr.user);
         if(tr.user == user.id) arr.push(tr);
       });
-      console.log(arr);
       res.render('./users/show', {user: req.user.id, userDetail: user, transactions: arr});
     });
   });
@@ -42,5 +33,3 @@ function edit (req, res) {
 function update (req, res) {
 
 }
-//5991e6ac9f8a45201602e37d
-// 5991eab4eecbfe224becd3ef
