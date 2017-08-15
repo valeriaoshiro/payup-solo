b74ef206f0efcf49b83ef64f4665a5c067d7fa65
Rosibel Cisneros <kelsocisneros@gmail.com>
2017-08-15 15:57:56 -0700

    added date dropdown

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
+var moment = require('moment');
 
 require('dotenv').config();
 
