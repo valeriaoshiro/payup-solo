db87d3065cc2823d664b5151250e9aa5a5bb36cd
Loretta Wang <l.wang32@gmail.com>
2017-08-15 10:29:51 -0700

    Added partial payments

--- routes/transactions.js
+++ routes/transactions.js
@@ -4,8 +4,10 @@ var transactionsController = require('../controllers/transactions');
 
 router.get('/new', transactionsController.new);
 router.post('/', transactionsController.create);
+router.get('/:id', transactionsController.show);
 router.get('/:id/edit', transactionsController.edit);
 router.put('/:id', transactionsController.update);
 router.delete('/:id', transactionsController.delete);
+router.post('/:id/payments', transactionsController.createPayment);
 
 module.exports = router; 
