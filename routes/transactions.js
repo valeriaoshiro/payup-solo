ef228b228810bf25caa0d2d905e570483152dd63
Rosibel Cisneros <kelsocisneros@gmail.com>
2017-08-15 17:54:50 -0700

    added edit to partial payments

--- routes/transactions.js
+++ routes/transactions.js
@@ -6,8 +6,10 @@ router.get('/new', transactionsController.new);
 router.post('/', transactionsController.create);
 router.get('/:id', transactionsController.show);
 router.get('/:id/edit', transactionsController.edit);
+router.get('/:id/:paymentId/edit', transactionsController.editPayment);
 router.put('/:id', transactionsController.update);
 router.delete('/:id', transactionsController.delete);
 router.post('/:id/payments', transactionsController.createPayment);
+router.put('/:id/:paymentId', transactionsController.updatePayment);
 
 module.exports = router; 
